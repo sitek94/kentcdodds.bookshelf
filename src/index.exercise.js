8c56edcf918b4da546d2f680749552d5a3966397
Maciek Sitkowski <maciej.sitkowski@netguru.com>
2022-06-05 13:57:38 +0200

    ex 1.2 - create `LoginForm` component

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -19,6 +19,10 @@ function App() {
   const openRegisterModal = () => setOpenModal(openModalState.register)
   const closeModal = () => setOpenModal(openModalState.none)
 
+  function login(formData) {
+    console.log('login', formData)
+  }
+
   return (
     <div>
       <Logo width="80" height="80" />
@@ -28,18 +32,59 @@ function App() {
       <Dialog
         aria-label="Login form"
         isOpen={openModal === openModalState.login}
-        onDismiss={closeModal}
       >
-        <h1>Login</h1>
+        <div>
+          <button onClick={closeModal}>Close</button>
+        </div>
+        <h3>Login</h3>
+        <LoginForm onSubmit={login} buttonText="Login" />
       </Dialog>
       <Dialog
         aria-label="Registration form"
         isOpen={openModal === openModalState.register}
-        onDismiss={closeModal}
       >
-        <h1>Register</h1>
+        <div>
+          <button onClick={closeModal}>Close</button>
+        </div>
+        <h3>Register</h3>
       </Dialog>
     </div>
+  )
+}
+
+function LoginForm({onSubmit, buttonText}) {
+  const [username, setUsername] = React.useState('')
+  const [password, setPassword] = React.useState('')
+
+  function handleSubmit(event) {
+    event.preventDefault()
+
+    onSubmit({username, password})
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          type="text"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          type="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+      </div>
+      <div>
+        <button type="submit">{buttonText}</button>
+      </div>
+    </form>
   )
 }
 
